5d1d2584768a03269a8f7b7eb7507c643557f795
Claude Lynch <124599703+siskiyoucedar@users.noreply.github.com>
2024-04-16 16:12:06 +0100

    minor graphing updates

--- 2_plotting.R
+++ 2_plotting.R
@@ -9,6 +9,7 @@ library(viridis)
 regions <- st_read("_Spatial_data/European_Electoral_Regions_Dec_2018_FCB_UK.shp")
 
 # unfortunately there's just one element that doesn't match.
+# East of England is named Eastern in this data even though they constitute the same area
 # this is a fudge, sorry!
 regions[6, 2] = "East of England"
 
@@ -23,6 +24,7 @@ plot_regions <- regions |> merge(
 # plotty
 
 breaks = c(0, 5, 10, 20, 30, 40)
+labels = c("0 to 5%", "5 to 10%", "10 to 20%", "20 to 30%", "30 to 40%")
 
 tm_shape(plot_regions) +
   tm_fill("%_raw", 
@@ -30,15 +32,16 @@ tm_shape(plot_regions) +
           title.color = "grey15",
           breaks = breaks,
           palette = "viridis",
-          alpha = 0.9
+          alpha = 0.9,
+          labels = labels
           ) +
   
-  tm_borders(col = "gray95") +
+  tm_borders(col = "gray5") +
   
   # tm_compass(position = c("left", "top")) +
   
   tm_layout(main.title = "% of stations with ticket barriers",
-            main.title.size = 1.2,
+            main.title.size = 1.35,
             main.title.fontfamily = "Arial",
             main.title.fontface = "bold",
             main.title.position = "center",
